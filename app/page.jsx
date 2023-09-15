528be18898ca5e1d91003c2619fdc796dce09356
Plycedes <abhaypratapyadav340@gmail.com>
2023-09-15 12:26:28 +0530

    File Structure and Header

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Explore & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            </h1>
+
+            <p className="desc text-center">
+                Zprompts is an open-source AI prompting tool for new-age to create, explore and
+                share creative prompts
+            </p>
+        </section>
+    );
+};
+
+export default Home;
